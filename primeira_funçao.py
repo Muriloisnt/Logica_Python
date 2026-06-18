89a0f46de4eddba5efa8334d90aa1148b81e9bd7
Muriloisnt <jardimm291@gmail.com>
2026-06-17 22:22:24 -0300

    17/06
    hoje aprendemos funções

---
+++ primeira_funçao.py
@@ -0,0 +1,27 @@
+
+
+
+
+def nome_da_funçao(parametro1, parametro2) :
+    """Docstring: desceve o que a fuçao faz"""
+    # Corpo da função
+    resultado = parametro1 + parametro2
+    return resultado
+# A palavra def inicia toda definição de função
+
+
+
+# Outro exemplo
+# Somar é o nome da minha função
+#  Retunr fica responsavel por:Retornar um valor: Ele envia o resultado de um processamento ou cálculo de volta para o ponto exato do programa onde a função foi chamada.
+# Encerrar a função: Ele para a execução da função imediatamente. Qualquer código escrito depois de um return ativo não será executado
+
+
+def somar(a, b):
+    return (a + b)
+
+resultado = somar (60, 7)
+print(resultado)
+
+
+# Docstring é a primeira string logo após o cabeçhalo de uma função.Ela descreve oque a função faz - e embora seja opcional,documentar é uma pratica essencial
